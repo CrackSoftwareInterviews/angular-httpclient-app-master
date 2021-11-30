48a42ab8947cff79a6f6d555dbfa63f2cea75152
CrackSoftwareInterviews <95309249+CrackSoftwareInterviews@users.noreply.github.com>
2021-12-01 00:28:38 +0530

    Add files via upload
    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { EmployeeCreateComponent } from './employee-create/employee-create.component';
+import { EmployeeEditComponent } from './employee-edit/employee-edit.component';
+import { UsersDetailComponent } from './users-detail/users-detail.component';
+import { UsersListComponent } from './users-list/users-list.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'create-employee' },
+  { path: 'create-employee', component: EmployeeCreateComponent },
+  { path: 'employee-edit/:id', component: EmployeeEditComponent },
+  { path: 'users-list', component: UsersListComponent },
+  { path: 'users-detail/:id', component: UsersDetailComponent },
+  { path: 'login', component: LoginComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
